7eb9d5d077f02ba123ecb2bd1b14176ad3177164
HaydenClark1 <143965948+HaydenClark1@users.noreply.github.com>
2024-05-08 10:44:38 -0500

    changed port

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     const sendDataToJava = async () => {
         try{
         //sending data to Spring Boot
-        const response = await fetch('http://localhost:8080/api/v1/user', {
+        const response = await fetch('http://18.222.253.68:8080/api/v1/user', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
